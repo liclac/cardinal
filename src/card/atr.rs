ce25c81e76bd8f07622eeed6715b9b146eb0bec8
embr <hi@liclac.eu>
2019-01-26 22:17:18 +0000

    Dropping AnswerToReset for now

--- src/card/atr.rs
+++
@@ -1,7 +0,0 @@
-pub struct AnswerToReset {}
-
-impl AnswerToReset {
-    fn parse(data: &[u8]) -> Self {
-        Self {}
-    }
-}
--- src/card/mod.rs
+++ src/card/mod.rs
@@ -1,6 +1,4 @@
-pub mod atr;
 pub mod card;
 pub mod commands;
 
-pub use self::atr::AnswerToReset;
 pub use self::card::Card;
