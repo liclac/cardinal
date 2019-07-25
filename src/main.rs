0b96aebba90770370ab10b48d989e8cd68a3ac2a
liclac <hi@liclac.eu>
2019-07-25 21:46:22 +0100

    cardinal, but every time you pass -v it gets noisier

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use error_chain::quick_main;
 use pcsc;
 use structopt::StructOpt;
-use tracing::{debug, span, Level};
+use tracing::{debug, info, span, trace, Level};
 
 mod errors {
     use error_chain::error_chain;
@@ -20,17 +20,22 @@ use errors::Result;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "cardinal", about = "The Swiss army knife of smartcards")]
 struct Opt {
-    #[structopt(short = "v", long = "verbose")]
-    /// Enable debug logging
-    verbose: bool,
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    /// Every time you -v, it gets noisier (up to -vvv)
+    verbosity: u8,
 }
 
 fn init_logging(opt: &Opt) -> Result<()> {
     Ok(tracing::subscriber::set_global_default(
         tracing_fmt::FmtSubscriber::builder()
             .with_filter(
-                tracing_fmt::filter::EnvFilter::try_new(if opt.verbose { "debug" } else { "info" })
-                    .unwrap(),
+                tracing_fmt::filter::EnvFilter::try_new(match opt.verbosity {
+                    0 => "warn",
+                    1 => "info",
+                    2 => "debug",
+                    _ => "trace",
+                })
+                .unwrap(),
             )
             .finish(),
     )
@@ -45,12 +50,18 @@ fn run() -> Result<()> {
     let _enter = span.enter();
 
     debug!("Connecting to PCSC...");
+    trace!("pcsc::Context::establish()");
     let ctx = pcsc::Context::establish(pcsc::Scope::User)?;
+    trace!("pcsc::Context::list_readers_len()");
     let mut reader_buf = Vec::with_capacity(ctx.list_readers_len()?);
     reader_buf.resize(reader_buf.capacity(), 0);
+    trace!(
+        { list_readers_len = reader_buf.capacity() },
+        "pcsc::Context::list_readers()"
+    );
     let readers = ctx.list_readers(&mut reader_buf)?;
     for name in readers {
-        println!("{}", name.to_str()?);
+        info!({ name = name.to_str()? }, "Found reader!");
     }
 
     Ok(())
