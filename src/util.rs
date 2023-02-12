d4798bf341f5d0ac8f80c2765c3ca4927ee999b0
embr <git@liclac.eu>
2023-02-13 00:24:49 +0100

    wiping and starting over

--- src/util.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-use std::io::prelude::*;
-
-/// Wrapper around Writer::write_all(), which still returns the number of bytes written.
-pub fn write_all<W: Write>(w: &mut W, buf: &[u8]) -> io::Result<usize> {
-    w.write_all(buf)?;
-    Ok(buf.len())
-}
